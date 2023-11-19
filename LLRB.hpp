30aea75c3664ae8283601136b16a4380f4b2dd73
abohoss <21p0141@eng.asu.edu.eg>
2023-11-19 16:38:33 +0200

    LLRB initiazlized

---
+++ LLRB.hpp
@@ -0,0 +1,24 @@
+#ifndef LLRB
+#define LLRB
+#include <iostream>
+using namespace std;
+template <typename V,typename K>
+class LLRB
+{
+private:
+Node* root;
+    class Node {
+        Node* left,right;
+        V val;
+        K key;
+        int size;
+        public:
+        Node(K key, V val, int size): left(0),right(0),val(val),key(key),size(size) {}
+    }
+public:
+    LLRB();
+    ~LLRB();
+    
+};
+
+
