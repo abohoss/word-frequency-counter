61ea38e2eafacfcc1a474b8090b585bd9edeff06
marwannour1 <marwannour20022016@gmail.com>
2023-12-27 15:14:31 +0200

    done

--- corpusMain.cpp
+++ corpusMain.cpp
@@ -1,25 +1,27 @@
-#include "LLRB.hpp"
-#include "main.hpp"
-#include <sstream>
-int main()
-{
-    // LLRB tree;
-    // string filepath = "eng_news_2005_100K-sentences.txt";
-    // tokenizeAndInsert(filepath, tree);
-    // string s = "your";
-    // int n = tree.getFrequency(s);
-    // cout << n << endl;
-    // long num = tree.getsize();
-    // cout << num << endl;
-    string name = "corpusTree.bin";
-    // tree.saveTree(name);
+// #include "LLRB.hpp"
+// #include "main.hpp"
+// #include <sstream>
+// int main()
+// {
+//     // LLRB tree;
+//     // string filepath = "eng_news_2005_100K-sentences.txt";
+//     // tokenizeAndInsert(filepath, tree);
+//     // string s = "we";
+//     // int n = tree.getFrequency(s);
+//     // cout << n << endl;
+//     // // cout << tree << endl;
+//     // long num = tree.getsize();
+//     // cout << num << endl;
+//     string name = "corpusTree.bin";
+//     // tree.saveTree(name);
 
-    LLRB tree2;
-    tree2.loadTree(name);
-    string s2 = "your";
-    int n2 = tree2.getFrequency(s2);
-    long num2 = tree2.getsize();
-    cout << n2 << endl;
-    cout << num2 << endl;
-    return 0;
-}
+//     LLRB tree2;
+//     tree2.loadTree(name);
+//     string s2 = "adjhfdfdfdfhasdjfhiawehfsdalkfhawe";
+//     int n2 = tree2.getFrequency(s2);
+//     long num2 = tree2.getsize();
+//     cout << n2 << endl;
+//     // cout << tree2 << endl;
+//     cout << num2 << endl;
+//     return 0;
+// }
